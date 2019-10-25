0a565f1298c0a20ab061107942be378433f3e171
DaniilTom <44425958+DaniilTom@users.noreply.github.com>
2019-10-25 15:41:54 +0300

    Angular

---
+++ MyProjects/Angular/SportsStore/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  //template: `<div class="bg-success p-a-1 text-xs-centre">This is sportsstore</div>`
+})
+export class AppComponent {
+  title = 'app works!';
+}
