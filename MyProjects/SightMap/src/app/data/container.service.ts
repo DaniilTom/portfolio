c7b81d4afd73a86ed2c84a9b43d3a7b1271aa765
DaniilTom <44425958+DaniilTom@users.noreply.github.com>
2019-11-06 10:05:48 +0300

    Angular Обновлен контейнер

--- MyProjects/SightMap/src/app/data/container.service.ts
+++ MyProjects/SightMap/src/app/data/container.service.ts
@@ -6,11 +6,20 @@ export class ContainerService {
     objArray: Pair[] = [];
 
     set(key: string, value: any) {
+        if(this.objArray.includes(this.get(key)))
+        {
+            this.remove(key);
+        }
         this.objArray.push(new Pair(key, value));
     }
 
     get(key: string) {
         return this.objArray.find(pair => pair.key == key).value;
+    }
+
+    remove(key: string){
+        var index = this.objArray.indexOf(this.get(key));
+        this.objArray.splice(index, 1);
     }
 }
 
