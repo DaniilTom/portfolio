44260faa9b8fb88507933dad085dd1b8e6478d61
DaniilTom <44425958+DaniilTom@users.noreply.github.com>
2019-11-15 17:12:41 +0300

    Update UploaderController.cs

--- MyProjects/SightMap/Controllers/Api/UploaderController.cs
+++ MyProjects/SightMap/Controllers/Api/UploaderController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using SightMap.BLL.PluploadManager;
+using static SightMap.Models.UploaderController;
 
 namespace SightMap.Controllers.Api
 {
